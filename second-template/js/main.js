2a5dc61d109603e6b1833b4f9cd919f58d87e90a
Ibrahim Rabah <95526530+IbrahimRabah@users.noreply.github.com>
2023-03-23 21:21:17 +0200

    Add files via upload

---
+++ second-template/js/main.js
@@ -0,0 +1,23 @@
+// var servicesScroll= $(".services").offset().top;
+
+// $(window).scroll(function(){
+//     var wscroll =$(window).scrollTop();
+//     var servicesScroll= $(".services").offset().top;
+//     if(wscroll > servicesScroll -50)
+//     {
+//         console.log("equals");
+//        $("header").css({'background-color':'red','postion':'fixed'})
+//     }
+// })
+
+// $(document).ready(function(){
+//     $("#loading .spinner").fadeOut(1000,
+//         ()=>{
+//             $("#loading .spinner").parent().fadeOut(1000,()=>{
+//                 $("#loading .spinner").remove();
+//                 $("body").css({'overflow-y':'auto'})
+//             })
+//         });
+
+//     });
+
